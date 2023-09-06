c063acb88528a80f3b181ff4c34fd519c31badee
darrenmoy <darrenm3060@gmail.com>
2023-09-06 14:00:23 -0400

    test

--- codebyte.py
+++ codebyte.py
@@ -2,9 +2,7 @@ def FirstFactorial(num):
     for i in range(num):
         nextnum = i - 1
         prod = i * nextnum
-        
-
-  return num
+    return num
 
 # keep this function call here
 print(FirstFactorial(5))
